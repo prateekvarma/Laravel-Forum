9a7b85c69f2dbbfdabeb7d7c7f2e6f921e20e6bf
unknown <prtk.varma@gmail.com>
2020-08-07 10:56:46 -0600

    Share Channel data to all views using AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Channel;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->share('channels', Channel::all() );
     }
 }
